a0026061bafaa45618e608d05b351ef4baf00520
Manuel Ocampo <75048521+ManuelOg16@users.noreply.github.com>
2021-03-20 16:22:06 -0500

    primer actualización
    se agregab los archivos paguna web personal

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,redirect,url_for,jsonify,json
+app = Flask(__name__)
+@app.route('/')                       
+def index():                           
+    return  render_template('index.html')
+
+@app.route('/proyectoplc')                       
+def plc():                           
+    return  render_template('proyectoplc.html')
+
+@app.route('/Scadaweb')                       
+def scada():                           
+    return  render_template('Scadaweb.html')
+
+@app.route('/contacto')                       
+def formulario():                           
+    return  render_template('contacto.html')
+
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=5100, debug=True)
