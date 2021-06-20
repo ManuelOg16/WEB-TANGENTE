048dc4f93d0a532241b05c7e3d72630865d2db1f
Manuel Ocampo <75048521+ManuelOg16@users.noreply.github.com>
2021-06-20 18:12:11 -0500

    actualización con 90% seo de web
    se agregaron metadescripciones, titulos  migas de pan, google analytics tag manager y google console search

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template,redirect,url_for,jsonify,json
+from flask import Flask, render_template, request, send_from_directory
 app = Flask(__name__)
+
 @app.route('/')                       
 def index():                           
     return  render_template('index.html')
@@ -8,24 +9,32 @@ def index():
 def quienes():                           
     return  render_template('quienes-somos.html')
 
-@app.route('/proyectos')                       
+@app.route('/proyectos/nuestros-proyectos')                       
 def exito():                           
-    return  render_template('proyectos.html')
+    return  render_template('/proyectos/nuestros-proyectos.html')
 
-@app.route('/proyecto-plc')                       
+@app.route('/proyectos/programacion-plc/plc-br')                       
 def plc():                           
-    return  render_template('proyecto-plc.html')
+    return  render_template('/proyectos/programacion-plc/plc-br.html')
 
-@app.route('/Scada-web')                       
+@app.route('/proyectos/scada-web/scada-web-formapol')                       
 def scada():                           
-    return  render_template('Scada-web.html')
-@app.route('/paginas-web')                       
+    return  render_template('/proyectos/scada-web/scada-web-formapol.html')
+@app.route('/proyectos/desarrollo-web/paginas-web')                       
 def paginas():                           
-    return  render_template('paginas-web.html')
+    return  render_template('/proyectos/desarrollo-web/paginas-web.html')
+
+@app.route('/proyectos/monitoreo/redes-electricas-potencia')                       
+def monitoreo():                           
+    return  render_template('/proyectos/monitoreo/redes-electricas-potencia.html')
 
 @app.route('/contacto')                       
 def formulario():                           
     return  render_template('contacto.html')
+
+@app.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 
 if __name__ == '__main__': 
